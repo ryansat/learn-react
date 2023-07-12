8ed6bf89d207c5af29630cf5994d1ff54a623a05
Fajar Satria <fajarsat@hotmail.com>
2023-07-13 02:43:00 +0700

    separate supabase project

--- chapter-6/supabase-crud/supabase-template/vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-});
